4d6d175db4646d6233acf787cd389fb5c81f86c9
rahug20 <62970058+rahug20@users.noreply.github.com>
2020-06-17 17:36:37 -0400

    Issue 13 (#21)
    * adding postgres and slo role

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,5 +1,5 @@
 variable "instance_count" {
-  default     = "6"
+  default     = "9"
 }
 
 variable "instance_type" {
@@ -8,12 +8,12 @@ variable "instance_type" {
 
 variable "instance_name" {
   type = list(string)
-  default = ["gm_control_api", "gm_control","gm_edge","gm_ijwt","gm_idata","gm_catalog"]
+  default = ["gm_control_api", "gm_control","gm_edge","gm_ijwt","gm_idata","gm_catalog","gm_slo","gm_dashboard","gm_postgresql"]
 }
 
 variable "cluster_tag" {
   type = list(string)
-  default = ["gm:cluster:control-api:8080", "control","gm:cluster:edge:8080","gm:cluster:internal-jwt-security:8080","gm:cluster:data-internal:8080","gm:cluster:catalog:8080"]
+  default = ["gm:cluster:control-api:8080", "control","gm:cluster:edge:8080","gm:cluster:internal-jwt-security:8080","gm:cluster:data-internal:8080","gm:cluster:catalog:8080","gm:cluster:slo:8080","gm:cluster:dashboard:8080","prostgres_db"]
 }
 
 variable "pub_sub_1_id" {}
